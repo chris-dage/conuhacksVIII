e1f73c42a35cd8ea9e555c00ecdc1d2bf2624f6a
unknown <chris.j.dage@gmail.com>
2024-01-20 15:00:42 -0500

    simple collision

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,23 +1,15 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var speed = 200
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func move(player, delta):
-	player.vel = Vector2()
 	if Input.is_key_pressed(KEY_D):
-		player.vel = Vector2(speed, 0)
-	if Input.is_key_pressed(KEY_A):
-		player.vel = Vector2(-speed, 0) 
-	if Input.is_key_pressed(KEY_W):
-		player.vel = Vector2(0, -speed) 
-	if Input.is_key_pressed(KEY_S):
-		player.vel = Vector2(0, speed) 
-	player.translate(player.vel * delta) # changing node's position
+		player.move_and_collide(Vector2(player.speed, 0))
+	elif Input.is_key_pressed(KEY_A):
+		player.move_and_collide(Vector2(-player.speed, 0)) 
+	elif Input.is_key_pressed(KEY_W):
+		player.move_and_collide(Vector2(0, -player.speed))
+	elif Input.is_key_pressed(KEY_S):
+		player.move_and_collide(Vector2(0, player.speed))
